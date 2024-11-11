74b5fa975da7ca79dc4b0ed33c57ee11dde6d311
sshriyasingh <ssingh5@terpmail.umd.edu>
2024-11-11 23:15:06 +0100

    all but progargs tidy issues fixed
    (cherry picked from commit 3ed8c5b1a3bdd79098934c92cd21be493723cae7)

--- utest-common/metadata_test.cpp
+++ utest-common/metadata_test.cpp
@@ -3,33 +3,37 @@
 #include "image_types.hpp"
 #include <gtest/gtest.h>
 
+constexpr static int HUND = 100;
+constexpr static int TWOHUND = 200;
+constexpr static int MAGICNUM = 255;
+
 // Test for get_metadata function
 TEST(MetadataTest, GetMetadata) {
     // Create an Image object with sample values
     Image image;
-    image.width = 100;
-    image.height = 200;
-    image.max_color_value = 255;
+    image.width = HUND;
+    image.height = TWOHUND;
+    image.max_color_value = MAGICNUM;
     image.pixels = std::vector<Pixel>(static_cast<size_t>(image.width) * static_cast<size_t>(image.height));  // Initialize pixel data
 
     // Call get_metadata to extract metadata
-    Metadata metadata = get_metadata(image);
+    const auto [width, height, maxColorValue] = get_metadata(image);
 
     // Check if the metadata values are correct
-    EXPECT_EQ(metadata.width, 100);
-    EXPECT_EQ(metadata.height, 200);
-    EXPECT_EQ(metadata.maxColorValue, 255);
+    EXPECT_EQ(width, 100);
+    EXPECT_EQ(height, 200);
+    EXPECT_EQ(maxColorValue, 255);
 }
 
 // Test for Metadata::toString function
 TEST(MetadataTest, ToString) {
     // Create a Metadata object with sample values
-    Metadata metadata;
-    metadata.width = 100;
-    metadata.height = 200;
-    metadata.maxColorValue = 255;
+    Metadata metadata{};
+    metadata.width = HUND;
+    metadata.height = TWOHUND;
+    metadata.maxColorValue = MAGICNUM;
 
     // Check if toString returns the correct formatted string
-    std::string expectedOutput = "Width: 100\nHeight: 200\nMax Color Value: 255";
+    std::string const expectedOutput = "Width: 100\nHeight: 200\nMax Color Value: 255";
     EXPECT_EQ(metadata.toString(), expectedOutput);
 }
