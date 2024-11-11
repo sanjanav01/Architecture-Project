d2a096e0e4f7e5ae9d0cc46b50c56d097627f388
Anchita <anchita14@gmail.com>
2024-11-11 15:21:28 +0100

    image aos and soa

--- imgaos/imageaos.hpp
+++ imgaos/imageaos.hpp
@@ -1,8 +1,9 @@
 #ifndef IMAGEAOS_HPP
 #define IMAGEAOS_HPP
 
-#include <vector>
+#include <map>
 #include <tuple>
+#include <vector>
 
 struct Pixel {
     int R;
@@ -21,8 +22,19 @@ public:
 
   // Function to remove infrequent colors
   void cutfreq(int frequency_threshold);
+  std::map<std::tuple<int, int, int>, int> calculateColorFrequencies() const;
+  std::vector<std::tuple<int, int, int>>
+      getInfrequentColors(std::map<std::tuple<int, int, int>, int> const & color_freq,
+                          int frequency_threshold) const;
+  void replaceInfrequentColors(std::map<std::tuple<int, int, int>, int> const & color_freq,
+                               std::vector<std::tuple<int, int, int>> const & infrequent_colors,
+                               int frequency_threshold);
+  std::tuple<int, int, int>
+      findClosestColor(std::tuple<int, int, int> const & color,
+                       std::map<std::tuple<int, int, int>, int> const & color_freq,
+                       int frequency_threshold) const;
 
-  private:
+private:
   // Helper function to find the closest replacement color
   std::tuple<int, int, int> findClosestColor(const std::tuple<int, int, int>& color,
                                              const std::map<std::tuple<int, int, int>, int>& color_freq,
--- imgsoa/imagesoa.hpp
+++ imgsoa/imagesoa.hpp
@@ -4,8 +4,8 @@
 #define IMAGESOA_HPP
 
 #include <map>
-#include <vector>
 #include <tuple>
+#include <vector>
 
 class ImageSOA {
 public:
@@ -20,9 +20,20 @@ public:
 
   // Function to remove infrequent colors
   void cutfreq(int frequency_threshold);
+  std::map<std::tuple<int, int, int>, int> calculateColorFrequencies() const;
+  std::vector<std::tuple<int, int, int>>
+      getInfrequentColors(std::map<std::tuple<int, int, int>, int> const & color_freq,
+                          int frequency_threshold) const;
+  void replaceInfrequentColors(std::map<std::tuple<int, int, int>, int> const & color_freq,
+                               std::vector<std::tuple<int, int, int>> const & infrequent_colors,
+                               int frequency_threshold);
+  std::tuple<int, int, int>
+      findClosestColor(std::tuple<int, int, int> const & color,
+                       std::map<std::tuple<int, int, int>, int> const & color_freq,
+                       int frequency_threshold) const;
   ImageSOA resize_soa(int new_width, int new_height) const;
 
-  private:
+private:
   // Helper function to find the closest replacement color
   std::tuple<int, int, int> findClosestColor(const std::tuple<int, int, int>& color,
                                              const std::map<std::tuple<int, int, int>, int>& color_freq,
