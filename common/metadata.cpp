eaf93f69bd9f72be444ade9f26fe0a552085ed8b
Sanjana <sanjanavellanki01@gmail.com>
2024-11-11 20:09:18 +0100

    metadata brackets

--- common/metadata.cpp
+++ common/metadata.cpp
@@ -2,7 +2,7 @@
 #include "metadata.hpp"
 
 Metadata get_metadata(const Image& image) {
-    Metadata metadata;
+    Metadata metadata {};
     metadata.width = image.width;
     metadata.height = image.height;
     metadata.maxColorValue = image.max_color_value;
