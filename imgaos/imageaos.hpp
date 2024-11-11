f8b288d3a148754162e76b26e87801e3f8cb84a4
Anchita <anchita14@gmail.com>
2024-11-11 15:05:24 +0100

    image soa/aos structs

--- imgaos/imageaos.hpp
+++ imgaos/imageaos.hpp
@@ -1,8 +1,36 @@
-//
-// Created by Shriya Singh on 11/5/24.
-//
+// imageaos.hpp
 
 #ifndef IMAGEAOS_HPP
 #define IMAGEAOS_HPP
 
-#endif //IMAGEAOS_HPP
+#include <vector>
+#include <tuple>
+
+struct Pixel {
+  int R;
+  int G;
+  int B;
+};
+
+class ImageAOS {
+  public:
+  std::vector<Pixel> pixels;
+  int width;
+  int height;
+
+  // Constructor to initialize the image dimensions
+  ImageAOS(int w, int h);
+
+  // Function to remove infrequent colors
+  void cutfreq(int frequency_threshold);
+
+  private:
+  // Helper function to find the closest replacement color
+  std::tuple<int, int, int> findClosestColor(const std::tuple<int, int, int>& color,
+                                             const std::map<std::tuple<int, int, int>, int>& color_freq,
+                                             int frequency_threshold);
+};
+
+#endif // IMAGEAOS_HPP
+
+
--- imgsoa/imagesoa.hpp
+++ imgsoa/imagesoa.hpp
@@ -1,8 +1,30 @@
-//
-// Created by Shriya Singh on 11/5/24.
-//
+// imagesoa.hpp
 
 #ifndef IMAGESOA_HPP
 #define IMAGESOA_HPP
 
-#endif //IMAGESOA_HPP
+#include <vector>
+#include <tuple>
+
+class ImageSOA {
+  public:
+  std::vector<int> R;
+  std::vector<int> G;
+  std::vector<int> B;
+  int width;
+  int height;
+
+  // Constructor to initialize the image dimensions
+  ImageSOA(int w, int h);
+
+  // Function to remove infrequent colors
+  void cutfreq(int frequency_threshold);
+
+  private:
+  // Helper function to find the closest replacement color
+  std::tuple<int, int, int> findClosestColor(const std::tuple<int, int, int>& color,
+                                             const std::map<std::tuple<int, int, int>, int>& color_freq,
+                                             int frequency_threshold);
+};
+
+#endif // IMAGESOA_HPP
