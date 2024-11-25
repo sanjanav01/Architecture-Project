5a1b50baf0beeba39164eceb6edae52cb83b3bf7
sshriyasingh <ssingh5@terpmail.umd.edu>
2024-11-25 22:44:38 +0100

    aos stuff added

--- imtool-aos/main.cpp
+++ imtool-aos/main.cpp
@@ -1,51 +1,51 @@
-// #include <iostream>
-// #include <string>
-// #include <vector>
-// #include "imgaos/imageaos.hpp"
-// #include "common/binaryio.hpp"
-// #include "common/metadata.hpp"
-//
-// constexpr static int MAX_MAXLEVEL = 65535;
-// constexpr static int FIVE = 5;
-// constexpr static int SIX = 6;
-//
-// int main(int const argc, char* argv[]) {
-//     if (argc < 4) {
-//     std::cerr << "Error: Insufficient arguments provided.\n";
-//     return 1;
-//   }
-//   std::vector<std::string> args(argv + 1, argv + argc);
-//     try {
-//         Image image = read_ppm(args[0]);
-//         if (args[2] == "info") {
-//             if (args.size() != 4) { throw std::invalid_argument("Invalid arguments for info");}
-//             get_metadata(image);
-//         } else if (args[2] == "maxlevel") {
-//             if (args.size() != FIVE) { throw std::invalid_argument("Invalid arguments for maxlevel"); }
-//             int const maxLevel = std::stoi(args[3]);
-//             if (maxLevel < 0 || maxLevel > MAX_MAXLEVEL) { throw std::out_of_range("maxlevel out of range");}
-//             ImageAOS::maxlevel(image, maxLevel);
-//             write_ppm(args[1], image);
-//         } else if (args[2] == "resize") {
-//             if (args.size() != SIX) { throw std::invalid_argument("Invalid arguments for resize"); }
-//             int const newWidth = std::stoi(args[3]);
-//             int const newHeight = std::stoi(args[4]);
-//             if (newWidth <= 0 || newHeight <= 0) { throw std::out_of_range("Invalid resize dimensions");}
-//             const Image resizedImage = ImageAOS::resize(image, newWidth, newHeight);
-//             write_ppm(args[1], resizedImage);
-//         } else if (args[2] == "cutfreq") {
-//             if (args.size() != FIVE) { throw std::invalid_argument("Invalid arguments for cutfreq"); }
-//             if (int const threshold = std::stoi(args[3]); threshold <= 0) { throw std::out_of_range("Invalid cutfreq threshold"); }
-//             // image.cutfreq(threshold); // Uncomment when implemented
-//             write_ppm(args[1], image);
-//         } else if (args[2] == "compress") {
-//             if (args.size() != 4) { throw std::invalid_argument("Invalid arguments for compress");}
-//             // CompressedImage compressedImage = compress_aos(image); // Uncomment when implemented
-//             // write_cppm(args[1], compressedImage); // Uncomment when implemented
-//         } else { throw std::invalid_argument("Invalid operation"); }
-//     } catch (const std::exception& e) {
-//         std::cerr << "Error: " << e.what() << '\n';
-//         return 1;
-//     }
-//     return 0;
-// }
+#include <iostream>
+#include <string>
+#include "imgaos/imageaos.hpp"
+#include "common/binaryio.hpp"
+constexpr static int MAX_ARGUMENTS_INFO = 3;
+constexpr static int MAX_ARGUMENTS_MAXLEVEL = 4;
+constexpr static int MAX_ARGUMENTS_RESIZE = 5;
+constexpr static int MAX_ARGUMENTS_CUTFREQ = 4;
+constexpr static int MAX_ARGUMENTS_COMPRESS = 3;
+
+int main(int const argc, char* argv[]) {
+    try {
+        std::vector<std::string> const arguments(argv + 1, argv + argc);
+        if (arguments.size() < 3) { std::cerr << "Error: Invalid number of arguments help: " << (arguments.size() - 1) << '\n'; exit(-1);}
+        const std::string& option = arguments[2];
+        auto validate_args = [&](int const expected) { if (arguments.size() != static_cast<size_t>(expected)) { std::cerr << "Error: Invalid number of arguments for " << option << (arguments.size()) << expected << '\n'; exit(-1); }};
+        if (option == "info") {
+            validate_args(MAX_ARGUMENTS_INFO);
+            Image const image = read_ppm(arguments[0]);
+            std::cout << "Width: " << image.width << ", Height: " << image.height << ", Max Color Value: " << image.max_color_value << '\n';
+        } else if (option == "maxlevel") {
+            validate_args(MAX_ARGUMENTS_MAXLEVEL);
+            int const newMaxValue = std::stoi(arguments[3]);
+            if (newMaxValue <= 0) { std::cerr << "Error: Invalid maxlevel value\n"; exit(-1);}
+            Image image = read_ppm(arguments[0]);
+            ImageAOS::maxlevel(image, newMaxValue);
+            write_ppm(arguments[1], image);
+        } else if (option == "resize") {
+            validate_args(MAX_ARGUMENTS_RESIZE);
+            int const newWidth = std::stoi(arguments[3]);
+            int const newHeight = std::stoi(arguments[4]);
+            if (newWidth <= 0 || newHeight <= 0) { std::cerr << "Error: Invalid resize dimensions\n"; exit(-1);}
+            Image const image = read_ppm(arguments[0]);
+            Image const resizedImage = ImageAOS::resize(image, newWidth, newHeight);
+            write_ppm(arguments[1], resizedImage);
+        } else if (option == "cutfreq") {
+            validate_args(MAX_ARGUMENTS_CUTFREQ);
+            int const frequencyThreshold = std::stoi(arguments[3]);
+            if (frequencyThreshold <= 0) { std::cerr << "Error: Invalid cutfreq value\n"; exit(-1);}
+            Image const image = read_ppm(arguments[0]);
+            ImageAOS imageAOS(image.width, image.height);
+            imageAOS.cutfreq(frequencyThreshold);
+            write_ppm(arguments[1], image);
+        } else if (option == "compress") {
+            validate_args(MAX_ARGUMENTS_COMPRESS);
+            write_cppm(arguments[1], compress_aos(read_ppm(arguments[0])));
+        } else if (option == "decompress") {
+            validate_args(MAX_ARGUMENTS_COMPRESS);
+            write_ppm(arguments[1], decompress(read_cppm(arguments[0])));
+        } else { std::cerr << "Error: Unknown option: " << option << '\n'; exit(-1);}
+    } catch (const std::exception& e) { std::cerr << "Unhandled exception: " << e.what() << '\n'; exit(-1); } return 0; }
